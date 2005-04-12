0ff2377f241e24527096ef4b15fdc21ed74193f1
emayor <emayor>
2005-04-12 11:16:01 +0000

    *** empty log message ***

---
+++ ServiceHandling/src/org/emayor/servicehandling/kernel/ForwardManager.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Apr 13, 2005
+ */
+package org.emayor.servicehandling.kernel;
+
+/**
+ * @author tku
+ */
+public class ForwardManager implements IForward {
+
+    /* (non-Javadoc)
+     * @see org.emayor.servicehandling.kernel.IForward#forwardRequest(org.emayor.servicehandling.kernel.ForwardMessage)
+     */
+    public void forwardRequest(ForwardMessage forwardMessage)
+            throws ForwardException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.emayor.servicehandling.kernel.IForward#forwardResponse(org.emayor.servicehandling.kernel.ForwardMessage)
+     */
+    public void forwardResponse(ForwardMessage forwardMessage)
+            throws ForwardException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
