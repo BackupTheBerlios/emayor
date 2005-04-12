b1409caa8c215fdceb061d6945c981ff1eb81712
emayor <emayor>
2005-04-12 03:49:01 +0000

    *** empty log message ***

--- ServiceHandling/src/org/emayor/servicehandling/kernel/IForward.java
+++ ServiceHandling/src/org/emayor/servicehandling/kernel/IForward.java
@@ -3,12 +3,14 @@
  */
 package org.emayor.servicehandling.kernel;
 
+import java.rmi.Remote;
+
 /**
  * @author <a href="mailto:Tomasz.Kusber@fokus.fraunhofer.de"><font size="-1">Tomasz Kusber</font></a><font size="-1"> FHI FOKUS (C)</font>
  */
-public interface IForward {
+public interface IForward extends Remote {
 	
-	public void forwardRequest(IForwardMessage forwardMessage) throws ForwardException;
+	public void forwardRequest(ForwardMessage forwardMessage) throws ForwardException;
 	
-	public void forwardReplay(IForwardMessage forwardMessage) throws ForwardException;
+	public void forwardReplay(ForwardMessage forwardMessage) throws ForwardException;
 }
