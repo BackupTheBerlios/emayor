ae51e798c3c9ed58cc6ef0ded95109dedc4e5bc9
emayor <emayor>
2005-06-01 04:28:19 +0000

    *** empty log message ***

---
+++ ServiceHandling/src/org/emayor/servicehandling/gui/admin/AdminManagerContollerServlet.java
@@ -0,0 +1,38 @@
+/*
+ * Created on Jun 2, 2005
+ */
+package org.emayor.servicehandling.gui.admin;
+
+import javax.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Class
+ *
+ * @web.servlet              name="AdminManagerContoller"
+ *                           display-name="Name for AdminManagerContoller"
+ *                           description="AdminManagerContoller"
+ * @web.servlet-mapping      url-pattern="/admin"
+ *
+ */
+public class AdminManagerContollerServlet extends HttpServlet {
+
+    /**
+     * 
+     */
+    public AdminManagerContollerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+}
