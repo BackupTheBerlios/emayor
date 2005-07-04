1fb5f62c48d52bb18cf4c7af048873f882a636fe
emayor <emayor>
2005-07-04 07:00:11 +0000

    When the JKS is not present a new one is created

--- PKCS12 to JKS/src/addPKCS12toJKS.java
+++ PKCS12 to JKS/src/addPKCS12toJKS.java
@@ -27,19 +27,37 @@ public class addPKCS12toJKS {
 			return;
 		}
 		// test
+		
+	try {	
 		java.security.Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		
+		// try to get the output keystorte
+		KeyStore outputKeyStore = KeyStore.getInstance("JKS");
+ 		
+		char[] nOPassword = args[1].toCharArray();
 		try {
+			FileInputStream ofis = new FileInputStream(args[0]);
+			outputKeyStore.load(ofis, nOPassword);
+			
+		} catch (FileNotFoundException myException){
+			// the jks file do not exist, so we create a new one
+			outputKeyStore.load(null, null);
+			System.out.println("The JKS keystore does not exists, so create a new one ...");
+			
+		}
+		
+			
+		
+		
+		
 			
 			KeyStore inputKeyStore = KeyStore.getInstance("PKCS12");
 			FileInputStream fis = new FileInputStream(args[2]);
 			char[] nPassword = args[3].toCharArray();
 			inputKeyStore.load(fis, nPassword);
 			fis.close();
-			KeyStore outputKeyStore = KeyStore.getInstance("JKS");
-			FileInputStream ofis = new FileInputStream(args[0]);
-			char[] nOPassword = args[1].toCharArray();
-			outputKeyStore.load(ofis, nOPassword);
+			
+			
 			
 			Enumeration enum = inputKeyStore.aliases();
 			
@@ -68,10 +86,13 @@ public class addPKCS12toJKS {
 			FileOutputStream out = new FileOutputStream(args[0]);
 			outputKeyStore.store(out, nOPassword);
 			out.close();
+
 		}
 		catch (Exception e){
 		e.printStackTrace();
 		}
+		
+					
 		
 		
 		
