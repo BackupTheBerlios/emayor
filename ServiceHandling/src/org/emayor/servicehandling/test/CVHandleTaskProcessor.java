17f0a983a7a287e82895d84af6043c38aa266a63
emayor <emayor>
2005-07-24 06:40:26 +0000

    *** empty log message ***

--- ServiceHandling/src/org/emayor/servicehandling/test/CVHandleTaskProcessor.java
+++ ServiceHandling/src/org/emayor/servicehandling/test/CVHandleTaskProcessor.java
@@ -20,67 +20,85 @@ import org.emayor.servicehandling.utclient.CivilServantTaskServiceClient;
  * @author tku
  */
 public class CVHandleTaskProcessor extends AbstractProcessor {
-    private static final Logger log = Logger
-            .getLogger(CVHandleTaskProcessor.class);
+	private static final Logger log = Logger
+			.getLogger(CVHandleTaskProcessor.class);
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.emayor.servicehandling.test.IProcessor#process(javax.servlet.http.HttpServletRequest,
-     *      javax.servlet.http.HttpServletResponse)
-     */
-    public String process(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        log.debug("-> start processing ...");
-        String ret = "Error.jsp";
-        try {
-            HttpSession session = req.getSession(false);
-            String asid = (String) session.getAttribute("ASID");
-            String ssid = (String) session.getAttribute("SSID");
-            String role = (String) session.getAttribute("ROLE");
-            if (log.isDebugEnabled()) {
-                log.debug("got asid : " + asid);
-                log.debug("got ssid : " + ssid);
-                log.debug("got role : " + role);
-            }
-            String action = req.getParameter("Submit");
-            if (log.isDebugEnabled())
-                log.debug("got action from request: " + action);
-            Task task = (Task) session.getAttribute("CURR_TASK");
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.emayor.servicehandling.test.IProcessor#process(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	public String process(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		log.debug("-> start processing ...");
+		String ret = "Error.jsp";
+		try {
+			HttpSession session = req.getSession(false);
+			String asid = (String) session.getAttribute("ASID");
+			String ssid = (String) session.getAttribute("SSID");
+			String role = (String) session.getAttribute("ROLE");
+			if (log.isDebugEnabled()) {
+				log.debug("got asid : " + asid);
+				log.debug("got ssid : " + ssid);
+				log.debug("got role : " + role);
+			}
+			String action = req.getParameter("Submit");
+			if (log.isDebugEnabled())
+				log.debug("got action from request: " + action);
+			Task task = (Task) session.getAttribute("CURR_TASK");
+			Task[] tasks = (Task[]) session.getAttribute("MY_TASKS");
 
-            if (action.equalsIgnoreCase("Approve")) {
-                log.debug("this is a case of approval");
-                if (task.getTaskType() == CVDocumentTypes.CV_USER_REGISTRATION_REQUEST)
-                	task.setStatus("APPROVED");
-                else task.setStatus("YES");
-            } else {
-                log.debug("this is a case of declination");
-                task.setStatus("NO");
-            }
-            CivilServantTaskServiceClient utm = new CivilServantTaskServiceClient();
-            log.debug("try to complete task");
-            utm.completeTask(asid, task);
+			if (action.equalsIgnoreCase("Approve")) {
+				log.debug("this is a case of approval");
+				if (task.getTaskType() == CVDocumentTypes.CV_USER_REGISTRATION_REQUEST)
+					task.setStatus("APPROVED");
+				else
+					task.setStatus("YES");
+			} else {
+				log.debug("this is a case of declination");
+				task.setStatus("NO");
+			}
+			CivilServantTaskServiceClient utm = new CivilServantTaskServiceClient();
+			log.debug("try to complete task");
+			utm.completeTask(asid, task);
 
-            session.setAttribute("SLEEP_TIME", "5");
-            session.setAttribute("REDIRECTION_URL",
-                    "ServiceHandlingTest?action=listMyTasks");
-            session.setAttribute("PAGE_TITLE", "Waiting for response ...");
-            session.setAttribute("REDIRECTION_TEXT",
-                    "The chosen task has been successfull finished!");
-            session.setAttribute("REDIRECTION_CANCEL_ACTION", "Welcome");
-            session.setAttribute("REDIRECTION_ACTION", "ServiceHandlingTest");
-            ret = "JustWait.jsp";
-            /**
-            Task[] tasks = utm.getMyTasks(asid);
-            tasks = Utils.setTypeToTasks(tasks);
-            session.setAttribute("MY_TASKS", tasks);
-            ret = "CVListTasks.jsp";
-            */
-        } catch (UserTaskException utex) {
-            log.error("caught ex: " + utex.toString());
-            // handle exception
-        }
-        log.debug("-> ... processing DONE!");
-        return ret;
-    }
+			/*
+			session.setAttribute("SLEEP_TIME", "5");
+			session.setAttribute("REDIRECTION_URL",
+					"ServiceHandlingTest?action=listMyTasks");
+			session.setAttribute("PAGE_TITLE", "Waiting for response ...");
+			session.setAttribute("REDIRECTION_TEXT",
+					"The chosen task has been successfull finished!");
+			session.setAttribute("REDIRECTION_CANCEL_ACTION", "Welcome");
+			session.setAttribute("REDIRECTION_ACTION", "ServiceHandlingTest");
+			ret = "JustWait.jsp";
+			*/
+			tasks = this.removeCompletedTask(tasks, task);
+			session.setAttribute("MY_TASKS", tasks);
+			ret = "CVListTasks.jsp";
+			/**
+			 * Task[] tasks = utm.getMyTasks(asid); tasks =
+			 * Utils.setTypeToTasks(tasks); session.setAttribute("MY_TASKS",
+			 * tasks); ret = "CVListTasks.jsp";
+			 */
+		} catch (UserTaskException utex) {
+			log.error("caught ex: " + utex.toString());
+			// handle exception
+		}
+		log.debug("-> ... processing DONE!");
+		return ret;
+	}
+
+	private final Task[] removeCompletedTask(Task[] tasks, Task completed) {
+		Task[] ret = new Task[tasks.length - 1];
+		int index = 0;
+		for (int i = 0; i < tasks.length; i++) {
+			if (tasks[i].getTaskId().equals(completed.getTaskId()))
+				log.debug("skipping this one: " + completed.getTaskId());
+			else
+				ret[index++] = tasks[i];
+		}
+		return ret;
+	}
 }
