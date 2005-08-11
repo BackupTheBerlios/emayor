731e127ad8afbb57790476af6de986a4e65e2438
emayor <emayor>
2005-08-11 10:37:24 +0000

    *** empty log message ***

--- ServiceHandling/src/org/emayor/servicehandling/test/ErrorData.java
+++ ServiceHandling/src/org/emayor/servicehandling/test/ErrorData.java
@@ -26,4 +26,100 @@ public class ErrorData {
 	
 	private String emailLink = "";
 
+	/**
+	 * @return Returns the emailLink.
+	 */
+	public String getEmailLink() {
+		return emailLink;
+	}
+	/**
+	 * @param emailLink The emailLink to set.
+	 */
+	public void setEmailLink(String emailLink) {
+		this.emailLink = emailLink;
+	}
+	/**
+	 * @return Returns the emailLinkEnabled.
+	 */
+	public boolean isEmailLinkEnabled() {
+		return emailLinkEnabled;
+	}
+	/**
+	 * @param emailLinkEnabled The emailLinkEnabled to set.
+	 */
+	public void setEmailLinkEnabled(boolean emailLinkEnabled) {
+		this.emailLinkEnabled = emailLinkEnabled;
+	}
+	/**
+	 * @return Returns the enableLink.
+	 */
+	public boolean isEnableLink() {
+		return enableLink;
+	}
+	/**
+	 * @param enableLink The enableLink to set.
+	 */
+	public void setEnableLink(boolean enableLink) {
+		this.enableLink = enableLink;
+	}
+	/**
+	 * @return Returns the errorMessage.
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	/**
+	 * @param errorMessage The errorMessage to set.
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	/**
+	 * @return Returns the errorTitle.
+	 */
+	public String getErrorTitle() {
+		return errorTitle;
+	}
+	/**
+	 * @param errorTitle The errorTitle to set.
+	 */
+	public void setErrorTitle(String errorTitle) {
+		this.errorTitle = errorTitle;
+	}
+	/**
+	 * @return Returns the linkLabel.
+	 */
+	public String getLinkLabel() {
+		return linkLabel;
+	}
+	/**
+	 * @param linkLabel The linkLabel to set.
+	 */
+	public void setLinkLabel(String linkLabel) {
+		this.linkLabel = linkLabel;
+	}
+	/**
+	 * @return Returns the linkLocation.
+	 */
+	public String getLinkLocation() {
+		return linkLocation;
+	}
+	/**
+	 * @param linkLocation The linkLocation to set.
+	 */
+	public void setLinkLocation(String linkLocation) {
+		this.linkLocation = linkLocation;
+	}
+	/**
+	 * @return Returns the pageTitle.
+	 */
+	public String getPageTitle() {
+		return pageTitle;
+	}
+	/**
+	 * @param pageTitle The pageTitle to set.
+	 */
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
 }
