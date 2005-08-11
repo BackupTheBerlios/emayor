4f523392effbdc5ee84255b77e869dc7c453c8bb
emayor <emayor>
2005-08-11 04:39:55 +0000

    *** empty log message ***

---
+++ ServiceHandling/src/org/emayor/servicehandling/gui/admin/pe/DoNothingProcessor.java
@@ -0,0 +1,36 @@
+/*
+ * $ Created on Aug 12, 2005 by tku $
+ */
+package org.emayor.servicehandling.gui.admin.pe;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.emayor.servicehandling.gui.admin.AbstractRequestProcessor;
+
+/**
+ * @author <a href="mailto:Tomasz.Kusber@fokus.fraunhofer.de"> <font
+ *         size="-1">Tomasz Kusber </font> </a> <font size="-1"> FHI FOKUS (C)
+ *         </font>
+ */
+public class DoNothingProcessor extends AbstractRequestProcessor {
+	private static final Logger log = Logger
+			.getLogger(DoNothingProcessor.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.emayor.servicehandling.gui.admin.IRequestProcessor#process(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	public String process(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		log.debug("-> start processing ...");
+		return "admpe/WelcomePage.jsp";
+	}
+
+}
--- ServiceHandling/src/org/emayor/servicehandling/test/StartServiceNewProcessor.java
+++ ServiceHandling/src/org/emayor/servicehandling/test/StartServiceNewProcessor.java
@@ -21,57 +21,72 @@ import org.emayor.servicehandling.utils.ServiceLocatorException;
  * @author tku
  */
 public class StartServiceNewProcessor extends AbstractProcessor {
-    private static final Logger log = Logger
-            .getLogger(StartServiceNewProcessor.class);
+	private static final Logger log = Logger
+			.getLogger(StartServiceNewProcessor.class);
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.emayor.servicehandling.test.IProcessor#process(javax.servlet.http.HttpServletRequest,
-     *      javax.servlet.http.HttpServletResponse)
-     */
-    public String process(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        String ret = "Error.jsp";
-        HttpSession session = req.getSession();
-        String asid = (String) session.getAttribute("ASID");
-        if (log.isDebugEnabled())
-            log.debug("got asid from session: " + asid);
-        String serviceName = req.getParameter("ServiceName");
-        if (log.isDebugEnabled())
-            log.debug("got service name: " + serviceName);
-        String role = (String) session.getAttribute("ROLE");
-        try {
-            ServiceLocator serviceLocator = ServiceLocator.getInstance();
-            AccessManagerLocal access = serviceLocator.getAccessManager();
-            String ssid = access.startService(asid, serviceName);
-            if (log.isDebugEnabled()) {
-                log.debug("got asid: " + asid);
-                log.debug("got ssid: " + ssid);
-                log.debug("got role: " + role);
-            }
-            session.setAttribute("SSID", ssid);
-            //session.setAttribute("SNAME",serviceName);
-            session.setAttribute("SLEEP_TIME", "10");
-            session.setAttribute("REDIRECTION_URL", "ServiceHandlingTest?action=GetInputDataPage");
-            session.setAttribute("PAGE_TITLE", "Waiting for input data page!");
-            session.setAttribute("REDIRECTION_TEXT", "Please wait a while - we are working for you!");
-            session.setAttribute("REDIRECTION_CANCEL_ACTION", "Welcome");
-            session.setAttribute("REDIRECTION_ACTION", "ServiceHandlingTest");
-            access.remove();
-            ret = "JustWait.jsp";
-        } catch (ServiceLocatorException ex) {
-            log.error("caught ex: " + ex.toString());
-            // TODO hadle exception
-        } catch (AccessException aex) {
-            log.error("caught ex: " + aex.toString());
-            // TODO handle ex
-        } catch (RemoveException rex) {
-            log.error("caught ex: " + rex.toString());
-            // TODO handle ex
-        }
-        log.debug("-> ... processing DONE!");
-        return ret;
-    }
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.emayor.servicehandling.test.IProcessor#process(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	public String process(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String ret = "Error.jsp";
+		HttpSession session = req.getSession();
+		String asid = (String) session.getAttribute("ASID");
+		if (log.isDebugEnabled())
+			log.debug("got asid from session: " + asid);
+		String serviceName = req.getParameter("ServiceName");
+		if (log.isDebugEnabled())
+			log.debug("got service name: " + serviceName);
+		String role = (String) session.getAttribute("ROLE");
+		try {
+			ServiceLocator serviceLocator = ServiceLocator.getInstance();
+			AccessManagerLocal access = serviceLocator.getAccessManager();
+			String ssid = access.startService(asid, serviceName);
+			if (log.isDebugEnabled()) {
+				log.debug("got asid: " + asid);
+				log.debug("got ssid: " + ssid);
+				log.debug("got role: " + role);
+			}
+			session.setAttribute("SSID", ssid);
+			//session.setAttribute("SNAME",serviceName);
+			if (serviceName.startsWith("AdminService")) {
+				log
+						.debug("the platfrom admin service has been started -> redirect the input page");
+				session.setAttribute("ADMIN_SSID", ssid);
+				ret = "admin/WelcomePage.jsp";
+			} else if (serviceName.startsWith("PolicyEnforcerAdminService")) {
+				log
+						.debug("the policy enforcer admin service has been started -> redirect the input page");
+				session.setAttribute("ADMIN_PE_SSID", ssid);
+				ret = "peadmin/WelcomePage.jsp";
+			} else {
+				session.setAttribute("SLEEP_TIME", "10");
+				session.setAttribute("REDIRECTION_URL",
+						"ServiceHandlingTest?action=GetInputDataPage");
+				session.setAttribute("PAGE_TITLE",
+						"Waiting for input data page!");
+				session.setAttribute("REDIRECTION_TEXT",
+						"Please wait a while - we are working for you!");
+				session.setAttribute("REDIRECTION_CANCEL_ACTION", "Welcome");
+				session.setAttribute("REDIRECTION_ACTION",
+						"ServiceHandlingTest");
+				ret = "JustWait.jsp";
+			}
+			access.remove();
+		} catch (ServiceLocatorException ex) {
+			log.error("caught ex: " + ex.toString());
+			// TODO hadle exception
+		} catch (AccessException ex) {
+			log.error("caught ex: " + ex.toString());
+			// TODO handle ex
+		} catch (RemoveException ex) {
+			log.error("caught ex: " + ex.toString());
+			// TODO handle ex
+		}
+		log.debug("-> ... processing DONE!");
+		return ret;
+	}
 }
