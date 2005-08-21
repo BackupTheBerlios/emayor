1163d6e87383622bb498c723df1e978b3c17882e
emayor <emayor>
2005-08-21 04:29:17 +0000

    *** empty log message ***

--- ServiceHandling/src/org/emayor/servicehandling/beans/AccessSessionEntityEJB.java
+++ ServiceHandling/src/org/emayor/servicehandling/beans/AccessSessionEntityEJB.java
@@ -31,156 +31,156 @@ import org.apache.log4j.Logger;
  *  
  */
 public abstract class AccessSessionEntityEJB implements EntityBean {
-	private final static Logger log = Logger
-			.getLogger(AccessSessionEntityEJB.class);
+    private final static Logger log = Logger
+            .getLogger(AccessSessionEntityEJB.class);
 
-	/**
-	 *  
-	 */
-	public AccessSessionEntityEJB() {
-		super();
-		log.debug("-> start processing ...");
-	}
+    /**
+     *  
+     */
+    public AccessSessionEntityEJB() {
+        super();
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#setEntityContext(javax.ejb.EntityContext)
-	 */
-	public void setEntityContext(EntityContext ctx) throws EJBException,
-			RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#setEntityContext(javax.ejb.EntityContext)
+     */
+    public void setEntityContext(EntityContext ctx) throws EJBException,
+            RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#unsetEntityContext()
-	 */
-	public void unsetEntityContext() throws EJBException, RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#unsetEntityContext()
+     */
+    public void unsetEntityContext() throws EJBException, RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#ejbRemove()
-	 */
-	public void ejbRemove() throws RemoveException, EJBException,
-			RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#ejbRemove()
+     */
+    public void ejbRemove() throws RemoveException, EJBException,
+            RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#ejbActivate()
-	 */
-	public void ejbActivate() throws EJBException, RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#ejbActivate()
+     */
+    public void ejbActivate() throws EJBException, RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#ejbPassivate()
-	 */
-	public void ejbPassivate() throws EJBException, RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#ejbPassivate()
+     */
+    public void ejbPassivate() throws EJBException, RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#ejbLoad()
-	 */
-	public void ejbLoad() throws EJBException, RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#ejbLoad()
+     */
+    public void ejbLoad() throws EJBException, RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.ejb.EntityBean#ejbStore()
-	 */
-	public void ejbStore() throws EJBException, RemoteException {
-		log.debug("-> start processing ...");
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.ejb.EntityBean#ejbStore()
+     */
+    public void ejbStore() throws EJBException, RemoteException {
+        log.debug("-> start processing ...");
+    }
 
-	/**
-	 * Getter for CMP Field asid
-	 * 
-	 * @ejb.pk-field
-	 * @ejb.persistent-field
-	 * @ejb.interface-method view-type="local"
-	 * @jboss.column-name name = "ASID"
-	 * @jboss.sql-type type = "VARCHAR(100)"
-	 * @jboss.jdbc-type type = "VARCHAR"
-	 * @jboss.persistence not-null = "true"
-	 */
-	public abstract java.lang.String getAsid();
+    /**
+     * Getter for CMP Field asid
+     * 
+     * @ejb.pk-field
+     * @ejb.persistent-field
+     * @ejb.interface-method view-type="local"
+     * @jboss.column-name name = "ASID"
+     * @jboss.sql-type type = "VARCHAR(100)"
+     * @jboss.jdbc-type type = "VARCHAR"
+     * @jboss.persistence not-null = "true"
+     */
+    public abstract java.lang.String getAsid();
 
-	/**
-	 * Setter for CMP Field asid
-	 * 
-	 * @ejb.interface-method view-type="local"
-	 */
-	public abstract void setAsid(java.lang.String value);
+    /**
+     * Setter for CMP Field asid
+     * 
+     * @ejb.interface-method view-type="local"
+     */
+    public abstract void setAsid(java.lang.String value);
 
-	/**
-	 * Create method
-	 * 
-	 * @ejb.create-method view-type = "local"
-	 */
-	public String ejbCreate(String asid) throws javax.ejb.CreateException {
-		log.debug("-> start processing ...");
-		this.setAsid(asid);
-		return null;
-	}
+    /**
+     * Create method
+     * 
+     * @ejb.create-method view-type = "local"
+     */
+    public String ejbCreate(String asid) throws javax.ejb.CreateException {
+        log.debug("-> start processing ...");
+        this.setAsid(asid);
+        return null;
+    }
 
-	/**
-	 * Post Create method
-	 */
-	public void ejbPostCreate(String asid) throws javax.ejb.CreateException {
-		log.debug("-> start processing ...");
-	}
+    /**
+     * Post Create method
+     */
+    public void ejbPostCreate(String asid) throws javax.ejb.CreateException {
+        log.debug("-> start processing ...");
+    }
 
-	/**
-	 * Getter for CMP Field userId
-	 * 
-	 * 
-	 * @ejb.persistent-field
-	 * @ejb.interface-method view-type="local"
-	 * @jboss.column-name name = "USERID"
-	 * @jboss.sql-type type = "VARCHAR(50)"
-	 * @jboss.jdbc-type type = "VARCHAR"
-	 */
-	public abstract String getUserId();
+    /**
+     * Getter for CMP Field userId
+     * 
+     * 
+     * @ejb.persistent-field
+     * @ejb.interface-method view-type="local"
+     * @jboss.column-name name = "USERID"
+     * @jboss.sql-type type = "VARCHAR(50)"
+     * @jboss.jdbc-type type = "VARCHAR"
+     */
+    public abstract String getUserId();
 
-	/**
-	 * Setter for CMP Field userId
-	 * 
-	 * @ejb.interface-method view-type="local"
-	 */
-	public abstract void setUserId(String value);
+    /**
+     * Setter for CMP Field userId
+     * 
+     * @ejb.interface-method view-type="local"
+     */
+    public abstract void setUserId(String value);
 
-	/**
-	 * Getter for CMP Field startDate
-	 * 
-	 * 
-	 * @ejb.persistent-field
-	 * @ejb.interface-method view-type="local"
-	 * @jboss.column-name name = "STARTDATE"
-	 * @jboss.sql-type type = "DATETIME"
-	 * @jboss.jdbc-type type = "TIMESTAMP"
-	 */
-	public abstract java.util.Date getStartDate();
+    /**
+     * Getter for CMP Field startDate
+     * 
+     * 
+     * @ejb.persistent-field
+     * @ejb.interface-method view-type="local"
+     * @jboss.column-name name = "STARTDATE"
+     * @jboss.sql-type type = "DATETIME"
+     * @jboss.jdbc-type type = "TIMESTAMP"
+     */
+    public abstract java.util.Date getStartDate();
 
-	/**
-	 * Setter for CMP Field startDate
-	 * 
-	 * @ejb.interface-method view-type="local"
-	 */
-	public abstract void setStartDate(java.util.Date value);
+    /**
+     * Setter for CMP Field startDate
+     * 
+     * @ejb.interface-method view-type="local"
+     */
+    public abstract void setStartDate(java.util.Date value);
 
 }
